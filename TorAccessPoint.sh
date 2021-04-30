f155411dc4bd3e3b604a566f3bba4ef37bd82fdc
Hugo <HJFR@users.noreply.github.com>
2021-04-30 18:13:18 +0100

    Create TorAccessPoint.sh

---
+++ TorAccessPoint.sh
@@ -0,0 +1,4 @@
+### [PT] Cofirmar que o OS esta actualizado.[/PT] [EN] Make sure the OS is up to date [/EN]
+sudo apt-get update && sudo apt-get upgrade
+### [PT] Instalar o git [/PT] [EN] Install git [/EN]
+sudo apt-get install git
